bd247b5241d43b67861fb20ea6477c5e092b691e
mscott2757 <mscott2757@berkeley.edu>
2018-01-30 02:14:01 -0800

    updated current photos a bit

--- src/content/Current.js
+++ src/content/Current.js
@@ -3,15 +3,16 @@ const Current = {
   content: {
     title: 'The Current Issue',
     photos: [
+      { url: require("../images/sf/bart.jpg"),            aspect: "6-by-7" },
+      { url: require("../images/sf/laundry.jpg"),         aspect: "3-by-2" },
       { url: require("../images/sf/sunset.jpg"),          aspect: "3-by-2" },
       { url: require("../images/sf/civic.jpg"),           aspect: "2-by-3" },
       { url: require("../images/sf/market.jpg"),          aspect: "6-by-6" },
       { url: require("../images/sf/mission.jpg"),         aspect: "3-by-2" },
       { url: require("../images/sf/muni.jpg"),            aspect: "2-by-3" },
-      { url: require("../images/berkeley/newspaper.jpg"), aspect: "7-by-6" },
-      { url: require("../images/berkeley/durant.jpg"),    aspect: "6-by-7" },
-      { url: require("../images/la/vinyl.jpg"),           aspect: "3-by-2" },
-      { url: require("../images/la/echo.jpg"),            aspect: "2-by-3" },
+      { url: require("../images/sf/car.jpg"),             aspect: "2-by-3" },
+      { url: require("../images/la/echo.jpg"),            aspect: "3-by-2" },
+      { url: require("../images/la/vinyl.jpg"),           aspect: "2-by-3" },
     ]
   }
 }
--- src/content/Landscapes.js
+++ src/content/Landscapes.js
@@ -3,9 +3,10 @@ const Landscapes = {
   content: {
     title: "Landscapes",
     photos: [
-      { url: require("../images/tam/empty.jpg"), aspect: "6-by-7" },
-      { url: require("../images/tam/road.jpg"),  aspect: "7-by-6" },
-      { url: require("../images/sf/ocean.jpg"),  aspect: "2-by-3" },
+      { url: require("../images/tam/empty.jpg"),  aspect: "6-by-7" },
+      { url: require("../images/tam/road.jpg"),   aspect: "7-by-6" },
+      { url: require("../images/sf/bernal.jpg"),  aspect: "2-by-3" },
+      { url: require("../images/sf/ocean.jpg"),   aspect: "3-by-2" },
     ]
   }
 }
