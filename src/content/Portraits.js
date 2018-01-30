ed2c1dab771999997fafb9e55a87a4dcf1da513e
mscott2757 <mscott2757@berkeley.edu>
2018-01-30 02:26:19 -0800

    fixed bug with loading aspect ratios

--- src/content/Portraits.js
+++ src/content/Portraits.js
@@ -3,12 +3,12 @@ const Portraits = {
   content: {
     title: "Portraits",
     photos: [
-      { url: require('../images/kyoto/bamboo-waves.jpg'),   aspect: '6-by-7' },
-      { url: require('../images/kyoto/waves-bridge.jpg'),   aspect: '6-by-7' },
-      { url: require('../images/osaka/castle-waves.jpg'),   aspect: '7-by-6' },
-      { url: require('../images/tokyo/harajuku-waves.jpg'), aspect: '6-by-7' },
-      { url: require("../images/tam/sunny.jpg"),            aspect: "6-by-7" },
-      { url: require("../images/tam/estell.jpg"),           aspect: "6-by-7" },
+      { url: require('../images/kyoto/bamboo-waves.jpg'),   aspect: '7-by-6' },
+      { url: require('../images/kyoto/waves-bridge.jpg'),   aspect: '7-by-6' },
+      { url: require('../images/osaka/castle-waves.jpg'),   aspect: '6-by-7' },
+      { url: require('../images/tokyo/harajuku-waves.jpg'), aspect: '7-by-6' },
+      { url: require("../images/tam/sunny.jpg"),            aspect: "7-by-6" },
+      { url: require("../images/tam/estell.jpg"),           aspect: "7-by-6" },
       { url: require("../images/paris/waves2.jpg"),         aspect: "2-by-3" },
       { url: require("../images/paris/waves.jpg"),          aspect: "3-by-2" },
       { url: require("../images/sf/deyoung.jpg"),           aspect: "3-by-2" },
