10c7e2f9047205f18a10aae0aa04bd5fe2257b53
mscott2757 <mscott2757@berkeley.edu>
2018-01-29 19:39:59 -0800

    added taiwan photos

--- src/content/Taiwan.js
+++ src/content/Taiwan.js
@@ -2,7 +2,25 @@ const Taiwan = {
   path: "/taiwan",
   content: {
     title: "Taiwan",
-    photos: []
+    photos: [
+      { url: require('../images/jiufen/jiufen.jpg'),      aspect: '7-by-6' },
+      { url: require('../images/jiufen/jiufen-3.jpg'),    aspect: '6-by-7' },
+      { url: require('../images/jiufen/jiufen-2.jpg'),    aspect: '7-by-6' },
+      { url: require('../images/jiufen/jioufen-4.jpg'),   aspect: '6-by-7' },
+      { url: require('../images/jiufen/jioufen.jpg'),     aspect: '7-by-6' },
+      { url: require('../images/jiufen/jioufen-5.jpg'),   aspect: '7-by-6' },
+      { url: require('../images/taipei/taipei-6.jpg'),    aspect: '7-by-6' },
+      { url: require('../images/taipei/m2-taipei-2.jpg'), aspect: '3-by-2' },
+      { url: require('../images/taipei/taipei-2.jpg'),    aspect: '6-by-7' },
+      { url: require('../images/taipei/taipei-3.jpg'),    aspect: '7-by-6' },
+      { url: require('../images/taipei/taipei-4.jpg'),    aspect: '7-by-6' },
+      { url: require('../images/taipei/m2-taipei-3.jpg'), aspect: '3-by-2' },
+      { url: require('../images/taipei/taipei-5.jpg'),    aspect: '6-by-7' },
+      { url: require('../images/taipei/m2-taipei.jpg'),   aspect: '3-by-2' },
+      { url: require('../images/taipei/m2-taipei-4.jpg'), aspect: '3-by-2' },
+      { url: require('../images/tamsui/tamsui-3.jpg'),    aspect: '6-by-7' },
+      { url: require('../images/tamsui/tamsui-4.jpg'),    aspect: '6-by-7' },
+    ]
   }
 }
 
