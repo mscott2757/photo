ab4681c26433bdf7406ec44b8a5133e33b6cb3b0
mscott2757 <mscott2757@berkeley.edu>
2018-06-09 23:38:36 -0700

    added react snapshot

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import { render } from 'react-snapshot';
 import { BrowserRouter } from 'react-router-dom';
 import './styles/css/index.css';
 import App from './App';
@@ -15,7 +15,7 @@ let store = createStore(
   applyMiddleware(thunkMiddleware)
 );
 
-ReactDOM.render((
+render((
   <Provider store={store}>
     <BrowserRouter>
       <App />
