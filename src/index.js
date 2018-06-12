3251b9b50994e58ae4a41513806d75357a4d90be
mscott2757 <mscott2757@berkeley.edu>
2018-06-12 01:44:24 -0700

    fixed something

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import reactDOM from 'react-dom';
+import ReactDOM from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
 import './styles/css/index.css';
 import App from './App';
