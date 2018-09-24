449ca3d24097c9dabbe1768c2a75c9a99ce3908a
mscott2757 <mscott2757@berkeley.edu>
2018-09-23 18:01:48 -0700

    simplify image component

--- src/components/Image.js
+++ src/components/Image.js
@@ -1,24 +1,24 @@
-import React, { Component } from 'react';
+import React from 'react';
+import { withStateHandlers } from 'recompose';
 import propTypes from 'prop-types';
 
-export class Image extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      loading: true
-    }
+const withLoading = withStateHandlers(
+  { loading: true },
+  {
+    hideLoader: ({ loading }) => () => ({ loading: !loading }),
   }
+);
 
-  hideLoader = () => {
-    this.setState({ loading: false });
-  }
-
-  render() {
-    const { aspect, url } = this.props.picture;
+export const Image = withLoading(
+  ({
+    loading,
+    hideLoader,
+    picture: { aspect, url },
+  }) => {
     let imgLoading = null;
-    if (this.state.loading) {
+    if (loading) {
       imgLoading = (
-        <div className={"image-loading aspect--" + aspect}>
+        <div className={`image-loading aspect--${aspect}`}>
           <i className="fa fa-circle-o-notch fa-spin"></i>
         </div>
       );
@@ -28,14 +28,14 @@ export class Image extends Component {
       <div className="image">
         {imgLoading}
         <img src={url}
-             className={(this.state.loading ? "hide-img" : "")}
-             onLoad={this.hideLoader}
+             className={(loading ? "hide-img" : "")}
+             onLoad={hideLoader}
              alt=""
         />
       </div>
     );
   }
-}
+)
 
 Image.propTypes = {
   picture: propTypes.shape({
--- src/components/Social.js
+++ src/components/Social.js
@@ -1,21 +1,19 @@
 import React from 'react';
 import propTypes from 'prop-types';
 
-export const Social = ({ accounts }) => {
-  return (
-    <div className="social">
-      <ul>
-        {accounts.map(({ url, className }, index) =>
-          <li key={index}>
-            <a href={url}>
-              <i className={`fa ${className}`}></i>
-            </a>
-          </li>
-        )}
-      </ul>
-    </div>
-  );
-}
+export const Social = ({ accounts }) => (
+  <div className="social">
+    <ul>
+      {accounts.map(({ url, className }, index) =>
+        <li key={index}>
+          <a href={url}>
+            <i className={`fa ${className}`}></i>
+          </a>
+        </li>
+      )}
+    </ul>
+  </div>
+);
 
 Social.propTypes = {
   accounts: propTypes.arrayOf(propTypes.shape({
