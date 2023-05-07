f7ddfc7161fea356e56169f4174c81c066e7103f
Mason Scott <mscott2757@gmail.com>
2023-05-06 22:55:00 -0700

    create theme

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import React from "react";
 import "./styles/app.scss";
+import { ThemeProvider } from 'styled-styled';
 import { Routes, useLocation, Navigate, Route } from "react-router-dom";
 import { TransitionGroup, CSSTransition } from "react-transition-group";
 import ReactGA from "react-ga";
 import { NavContainer } from "./containers";
 import { Accounts } from "./content/";
 import { Social, Main } from "./components";
+import { AppContextProvider } from "./context";
+import { theme } from './theme';
 
 ReactGA.initialize("UA-109516063-1");
 
@@ -17,14 +20,18 @@ const LogPageView = () => {
 
 export default () => {
   return (
-    <div className="App">
-      <NavContainer />
-      <Routes>
-        <Route element={<LogPageView />} />
-        <Route exact path="/" element={<Navigate replace to="/current" />} />
-        <Route path="/:id?" element={<Main />} />
-      </Routes>
-      <Social accounts={Accounts} />
-    </div>
+    <AppContextProvider>
+      <ThemeProvider theme={theme}>
+      <div className="App">
+        <NavContainer />
+        <Routes>
+          <Route element={<LogPageView />} />
+          <Route exact path="/" element={<Navigate replace to="/current" />} />
+          <Route path="/:id?" element={<Main />} />
+        </Routes>
+        <Social accounts={Accounts} />
+      </div>
+      </ThemeProvider>
+    </AppContextProvider>
   );
 };
---
+++ src/theme.js
@@ -0,0 +1,11 @@
+import { css } from 'styled-components';
+
+export const theme = {
+  height: '80vh',
+  width: '85vw',
+  colors: {
+    lightGrey: '#a8a8a8',
+    black: '#333',
+    white: '#fff',
+  },
+};
